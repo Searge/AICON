8f5e72f04ff9a28ff22edbd458cca6eb2eb773a5
Searge <ashikcygan@gmail.com>
2019-02-13 15:42:33 +0200

    Read File -> 4nd iter

--- readfile.py
+++ readfile.py
@@ -1,18 +1,14 @@
-import re
-
 with open('svg_text.txt', 'r') as f:
     array = [row.strip().split() for row in f]
 
 print(array)
 
-re.findall(r"/[\w'._±]+@[\w'._±]+/", array)
-
 array = []
 with open('svg.txt') as f:
     array = [''.join(row.split()) for row in f]
 
-        #for word in row:
-            #array = [word.split()]
+        # for word in row:
+            # array = [word.split()]
 
 print(array)
 
@@ -20,6 +16,6 @@ array2 = []
 for word in array:
     for i in word:
         array2.append(i)
-    #array2 = word.split()
+    # array2 = word.split()
 
 print(array2)
