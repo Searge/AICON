8ab7bc0d483435b80d2ed0b57c1cf5b446a58067
Searge <ashikcygan@gmail.com>
2019-02-16 13:02:25 +0200

    Mutation >> Beta

--- mutatio.py
+++ mutatio.py
@@ -4,11 +4,14 @@ array = [['веле', 'лелю', 'лювю', 'вюве'],
          ['жежю', 'вілі']]
 
 mutatio = []
+out = open('new.txt', 'w')
 
 for elem in array:
     mutatio.append(list(permutations(elem)))
     for item in mutatio:
         for tup in item:
-            print(' '.join(tup))
+            out.write(' '.join(tup))
+            out.write('\n')
 
+out.close()
 # print(mutatio)
