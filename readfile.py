124d0f6fd94ad45c5188589672d4bbc6f6c1c28e
Searge <ashikcygan@gmail.com>
2019-02-19 12:27:33 +0200

    :sparkles: 2

--- readfile.py
+++ readfile.py
@@ -1,16 +1,21 @@
 from itertools import permutations
+import time
+start = time.time()
 
+# ЗМІНИТИ НАЗВУ ФАЙЛУ НА ПОТРІБНУ
 with open('text.txt') as f:
     array = [row.split() for row in f]
 
 mutatio = []
 out = open('new.txt', 'w')
 
-for elem in array:
-    mutatio.append(list(permutations(elem)))
+for lst in array:
+    mutatio.append(list(permutations(lst)))
     for item in mutatio:
         for tup in item:
             out.write(' '.join(tup))
             out.write('\n')
-
 out.close()
+
+end = time.time()
+print(f'Операція зайняла {end - start} секунд')
