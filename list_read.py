82fb349452edab04aba2d73d3206973757b1d9b2
Searge <ashikcygan@gmail.com>
2019-03-02 22:43:40 +0200

    list mutations 2.1

--- list_read.py
+++ list_read.py
@@ -6,12 +6,13 @@ for lst in array:
     for word in lst:
         lst_of_two = [word[letter:letter + 2]
                       for letter in range(0, len(word), 2)]
-        for double in lst_of_two:
-            a, b = double
-            permutl.extend([b, a])
+        print(lst_of_two)
+        # for double in lst_of_two:
+        #     a, b = double
+        #     permutl.extend([b, a])
 
 # print(array)
-lst_of_four = [permutl[i:i+4] for i in range(0, len(permutl), 4)]
-# Склеюємо значення по 4 в окремий список
-for i in [j for j in lst_of_four]:
-    print(i)
+# lst_of_four = [permutl[i:i+4] for i in range(0, len(permutl), 4)]
+# # Склеюємо значення по 4 в окремий список
+# for i in [j for j in lst_of_four]:
+#     print(i)
