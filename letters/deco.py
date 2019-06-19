a521017f8c2cf4404ce9dec7e46078fe70d7fd29
Searge <ashikcygan@gmail.com>
2019-06-19 15:16:01 +0300

    Generate text SVG :zap:

--- letters/deco.py
+++ letters/deco.py
@@ -44,7 +44,7 @@ def svg(func):
 
 def defs(func):
     defs_o = "<defs><style>"
-    style_o = "svg {background-color: white; font: bold 2.1em; opacity: 0.03;}"
+    style_o = "svg {background-color: white; font: bold 3.2em; fill-opacity: 0.12;}"
     style_c = "</style>"
     text = f"""<text id="letter" text-anchor="middle" x="32" y="32">{letter}
             </text>"""
